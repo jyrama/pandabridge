ae8301092de1bfebca5861b4288a1d26d2ef696b
Jyri Genral <jyri.genral@protonmail.ch>
2021-06-02 00:47:55 +0300

    Start new Matrix.User class
    Signed-off-by: Jyri Genral <jyri.genral@protonmail.ch>

---
+++ src/main/java/fi/jrd/pandabridgemod/matrix/User.java
@@ -0,0 +1,48 @@
+package fi.jrd.pandabridgemod.matrix;
+
+import java.io.IOException;
+
+import com.google.gson.Gson;
+
+import org.apache.hc.client5.http.fluent.Request;
+import org.apache.hc.client5.http.fluent.Response;
+
+import fi.jrd.pandabridgemod.PandabridgeMod;
+
+public class User {
+    private Homeserver server;
+    private static final Gson GSON = new Gson();
+
+    private Profile profile;
+
+    User(String id, Homeserver server) {
+        this.server = server;
+
+        String profileUrl = String.format("%s_matrix/client/r0/profile/%s", this.server.getAddress(), id);
+
+        try {
+            Response res = Request.get(profileUrl).addHeader("Authorization", PandabridgeMod.authorization).execute();
+            if (res.returnResponse().getCode() != 200) {
+                PandabridgeMod.logger.error("Failed to construct User id {}: {}", id,
+                        res.returnResponse().getReasonPhrase());
+            }
+
+            this.profile = GSON.fromJson(res.returnContent().asString(), Profile.class);
+        } catch (IOException e) {
+            PandabridgeMod.logger.error("IO error while constructing user id {}: {}", id, e);
+        }
+    }
+
+    public String getDisplayName() {
+        return this.profile.displayname;
+    }
+
+    public String getAvatarURL() {
+        return this.profile.avatar_url;
+    }
+
+    class Profile {
+        private String displayname;
+        private String avatar_url;
+    }
+}
