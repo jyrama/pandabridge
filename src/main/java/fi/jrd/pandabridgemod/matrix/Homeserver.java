cba9d3b32b5a6e6660bb54c8072ef9cbe8c59861
Jyri Genral <jyri.genral@protonmail.ch>
2021-06-02 21:08:13 +0300

    Add uploadImage method to Homeserver
    Signed-off-by: Jyri Genral <jyri.genral@protonmail.ch>

--- src/main/java/fi/jrd/pandabridgemod/matrix/Homeserver.java
+++ src/main/java/fi/jrd/pandabridgemod/matrix/Homeserver.java
@@ -1,6 +1,21 @@
 package fi.jrd.pandabridgemod.matrix;
 
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+import java.io.InputStream;
+
+import javax.imageio.ImageIO;
+
+import com.google.gson.Gson;
+
+import org.apache.hc.client5.http.fluent.Request;
+import org.apache.hc.client5.http.fluent.Response;
+
+import fi.jrd.pandabridgemod.PandabridgeMod;
+
 public class Homeserver {
+    private static final Gson GSON = new Gson();
+
     private String address;
 
     public Homeserver(String address) {
@@ -17,5 +32,27 @@ public class Homeserver {
 
     public String getAddress() {
         return address;
+    }
+
+    public String uploadImage(BufferedImage img) {
+        String url = String.format("%s/_matrix/media/r0/upload", this.address);
+        InputStream imgInput = null;
+        try {
+            imgInput = (InputStream) ImageIO.createImageInputStream(img);
+            Response httpRes = Request.post(url).bodyStream(imgInput).execute();
+            if (httpRes.returnResponse().getCode() != 200) {
+                PandabridgeMod.logger.error("Uploading image failed: {}", httpRes.returnResponse().getReasonPhrase());
+            }
+            ContentUriResponse res = GSON.fromJson(httpRes.returnContent().asString(), ContentUriResponse.class);
+            return res.content_uri;
+        } catch (IOException e) {
+            PandabridgeMod.logger.error("IOException during image upload: {}", e);
+        }
+
+        return null;
+    }
+
+    class ContentUriResponse {
+        private String content_uri;
     }
 }
